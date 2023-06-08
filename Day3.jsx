201edc91de0d312a540f5fcc36e29c06452a3199
Yakoob Hammouri <jakoub-h2002@hotmail.com>
2023-06-08 10:34:14 +0300

    Update Day3.jsx

--- Day3.jsx
+++ Day3.jsx
@@ -1,1 +1,7 @@
 
+const persons = [
+    {name : "Ali", email :"user1@on.com"},
+    {name : "yakoob", email :"user2@on.com"},
+    {name : "Ahmad", email :"user3@on.com"},
+    {name : "mohammed", email :"user4@on.com"}
+  ]
