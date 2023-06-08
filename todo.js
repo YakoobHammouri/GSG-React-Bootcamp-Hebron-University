aff4bf55d964a4e0b30a6b13f2ac394e51be707b
Yakoob Hammouri <jakoub-h2002@hotmail.com>
2023-06-08 14:44:27 +0300

    Update todo.js

--- todo.js
+++ todo.js
@@ -1,11 +1,35 @@
 import React, { useEffect, useState } from "react";
 const Todo = () => {
+  const [task, setTask] = useState("");
+  const [todoItems, setTodoItems] = useState([]);
+  const gteTaskText = (event) => {
+    setTask(event.target.value);
+  };
+
+  const addTask = () => {
+    const taskObj = {
+      id: Math.random() * 100,
+      text: task,
+      isDone: false,
+      isDelete: false,
+    };
+    setTodoItems([...todoItems, taskObj]);
+  };
+
+  console.log("todoItems : ", todoItems);
   return (
     <>
       <h1> ToDo App</h1>
       <div className="add-task">
-        <input type="text" id="task" placeholder="Add a new task" />
-        <button id="add-task">Add</button>
+        <input
+          type="text"
+          id="task"
+          placeholder="Add a new task"
+          onChange={gteTaskText}
+        />
+        <button id="add-task" onClick={addTask}>
+          Add
+        </button>
       </div>
       <ul>
         <li>
