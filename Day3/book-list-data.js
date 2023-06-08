d2bc4fb8336cdad8e4d9cc646cfe6d558ccc1750
YakoobHammouri <jakoub-h2002@hotmail.com>
2023-06-09 01:03:54 +0300

    book list

---
+++ Day3/book-list-data.js
@@ -0,0 +1,73 @@
+const books = [
+    {
+      title: "To Kill a Mockingbird",
+      author: "Harper Lee",
+      publicationYear: 1960,
+      rating: 4.27,
+      language: "English"
+    },
+    {
+      title: "1984",
+      author: "George Orwell",
+      publicationYear: 1949,
+      rating: 4.17,
+      language: "English"
+    },
+    {
+      title: "The Great Gatsby",
+      author: "F. Scott Fitzgerald",
+      publicationYear: 1925,
+      rating: 3.91,
+      language: "English"
+    },
+    {
+      title: "Pride and Prejudice",
+      author: "Jane Austen",
+      publicationYear: 1813,
+      rating: 4.25,
+      language: "English"
+    },
+    {
+      title: "The Catcher in the Rye",
+      author: "J.D. Salinger",
+      publicationYear: 1951,
+      rating: 3.81,
+      language: "English"
+    },
+    {
+      title: "To the Lighthouse",
+      author: "Virginia Woolf",
+      publicationYear: 1927,
+      rating: 3.79,
+      language: "English"
+    },
+    {
+      title: "Crime and Punishment",
+      author: "Fyodor Dostoevsky",
+      publicationYear: 1866,
+      rating: 4.21,
+      language: "Russian"
+    },
+    {
+      title: "One Hundred Years of Solitude",
+      author: "Gabriel Garcia Marquez",
+      publicationYear: 1967,
+      rating: 4.06,
+      language: "Spanish"
+    },
+    {
+      title: "The Lord of the Rings",
+      author: "J.R.R. Tolkien",
+      publicationYear: 1954,
+      rating: 4.5,
+      language: "English"
+    },
+    {
+      title: "Harry Potter and the Sorcerer's Stone",
+      author: "J.K. Rowling",
+      publicationYear: 1997,
+      rating: 4.47,
+      language: "English"
+    }
+  ];
+  
