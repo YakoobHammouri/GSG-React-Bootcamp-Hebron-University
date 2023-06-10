14acddae378e7fa64dfb16eb099ca728f612febb
Yakoob Hammouri <jakoub-h2002@hotmail.com>
2023-06-10 12:20:28 +0300

    Update App.js

--- Day4/gsg-shop/src/App.js
+++ Day4/gsg-shop/src/App.js
@@ -1,6 +1,14 @@
+import { useState } from "react";
 import "./App.css";
+import Login from "./page/Login";
+import ProductPage from "./page/productPage";
 function App() {
-  return <div className="App"></div>;
+  const [isLogin, setIsLogin] = useState(false);
+  return (
+    <div className="App">
+      {isLogin === false ? <Login setIsLogin={setIsLogin} /> : <ProductPage />}
+    </div>
+  );
 }
 
 export default App;
