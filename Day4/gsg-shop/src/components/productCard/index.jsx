2338e3def9885fba92e45f2628a0106a23218cde
Yakoob Hammouri <jakoub-h2002@hotmail.com>
2023-06-10 14:32:28 +0300

    Update index.jsx

--- Day4/gsg-shop/src/components/productCard/index.jsx
+++ Day4/gsg-shop/src/components/productCard/index.jsx
@@ -1,10 +1,36 @@
 import React from "react";
 import "./style.css";
 const ProductCard = (props) => {
-  const { id, title, description, price, rating, brand, category, thumbnail } =
-    props?.product;
+  // const { id, title, description, price, rating, brand, category, thumbnail } =
+  //   props?.product;
 
-  return <div className="product"></div>;
+  const { product } = props;
+  if (product) {
+    const {
+      id,
+      title,
+      description,
+      price,
+      rating,
+      brand,
+      category,
+      thumbnail,
+    } = product;
+  }
+
+  return (
+    <div className="product">
+      <img src={thumbnail} alt={title} className="product-thumbnail" />
+      <h2 className="product-title">{title}</h2>
+      <p className="product-description">{description}</p>
+      <div className="product-details">
+        <p className="product-price">${price}</p>
+        <p className="product-rating">Rating: {rating}</p>
+        <p className="product-brand">Brand: {brand}</p>
+        <p className="product-category">Category: {category}</p>
+      </div>
+    </div>
+  );
 };
 
 export default ProductCard;
