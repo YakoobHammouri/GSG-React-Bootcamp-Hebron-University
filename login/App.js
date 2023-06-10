dade14aecf4f400cfe13df2b8d63ed373d59f568
Yakoob Hammouri <jakoub-h2002@hotmail.com>
2023-06-10 12:18:07 +0300

    Add files via upload

---
+++ login/App.js
@@ -0,0 +1,11 @@
+import "./App.css";
+import ProductPage from "./page/productPage";
+function App() {
+  return (
+    <div className="App">
+      <ProductPage />
+    </div>
+  );
+}
+
+export default App;
